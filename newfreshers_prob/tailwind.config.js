f826401698c3b84dd68869b2b9940e74a39993ba
Yaswanth <yaswanthram.396@gmail.com>
2024-12-05 15:39:12 +0530

    first commit

---
+++ newfreshers_prob/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Include .tsx files here
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
